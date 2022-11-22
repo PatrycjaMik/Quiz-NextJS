c561dbdd947e1fc39947e27ddfef141ff5489537
Patrycja Mikuka <patrycja1.mikula@gmail.com>
2022-11-22 22:13:28 +0100

    prize section cd

--- components/Prize/index.jsx
+++ components/Prize/index.jsx
@@ -8,24 +8,30 @@ export default function Prize() {
         <Image src="/images/topRightBlackDots.png" layout="fill" />
       </div>
 
-      <SideBorderBox containerClass=" h-[550px]" inverted={true}>
-        <div className="pt-10 w-[80%] m-auto">
-          <div className="flex flex-col">
-            <p className="font-oswald font-[600] text-2xl tracking-[0.62px] ">
-              Nagroda w konkursie:
-            </p>
-            <p className=" mt-5 mb-2 font-oswald font-[400] text-2xl text-white tracking-[0.52px] ">
-              TechniSat DIGITRADIO BT1
-            </p>
-            <p className="font-oswald  text-base tracking-[0.52px] ">
-              nowoczesny głośnik Bluetooth z radiem cyfrowym DAB+ i FM
-            </p>
+      <div className=" pt-16 w-[80%] m-auto">
+        <SideBorderBox containerClass=" h-[550px]" inverted={true}>
+          <div className="pt-10 w-[80%] m-auto">
+            <div className="flex flex-col px-5">
+              <p className="font-oswald  font-[600] text-2xl tracking-[0.62px] ">
+                Nagroda w konkursie:
+              </p>
+              <p className=" mt-5 mb-2 font-oswald font-[400] text-2xl text-white tracking-[0.52px] ">
+                TechniSat DIGITRADIO BT1
+              </p>
+              <p className="font-oswald  text-base tracking-[0.52px] ">
+                nowoczesny głośnik Bluetooth z radiem cyfrowym DAB+ i FM
+              </p>
+            </div>
+            <div className="absolute">
+              <Image
+                src="/images/radio.png"
+                width="268px"
+                height="259px"
+              ></Image>
+            </div>
           </div>
-          <div className="absolute">
-            <Image src="/images/radio.png" width="268px" height="259px"></Image>
-          </div>
-        </div>
-      </SideBorderBox>
+        </SideBorderBox>
+      </div>
 
       <div className="absolute  bottom-0 left-0 w-10/12 aspect-[249/138] ">
         <Image src="/images/bottomLeftBlackDots.png" layout="fill" />
