321e549b96af7dce913f58b386287c5cfb8fa6f8
Marcin Łapiński <marcin.lapinski05@gmail.com>
2022-11-22 16:12:51 +0100

    complete yellow menu

--- components/YellowMenu/YellowMenu.jsx
+++ components/YellowMenu/YellowMenu.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import styles from "./styles/YellowMenu.module.scss"
 
 const YellowMenu = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <>
         <div className={styles.YMenuContainer}>
@@ -46,10 +49,41 @@ const YellowMenu = () => {
                         <div className={styles.socYt}></div>
                     </a>
                 </div>
-                <div className={styles.moreButton}>
+                <div 
+                    className={styles.moreButton}
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    <div className={styles.arrowLeft} style={{transform: menuOpen ? "rotate(90deg)" : "none"}}></div>
                     więcej
                 </div>
             </div>
+            {menuOpen &&
+                <nav className={styles.navigationMobile}>
+                    <a href="https://www.polskieradio.pl/10/8387" className={styles.navLink}>
+                        newsy
+                    </a>
+                    <div className={styles.decorDot}></div>
+                    <a href="https://www.polskieradio.pl/10/214" className={styles.navLink}>
+                        muzyka
+                    </a>
+                    <div className={styles.decorDot}></div>
+                    <a href="https://www.polskieradio.pl/10/216" className={styles.navLink}>
+                        kultura
+                    </a>
+                    <div className={styles.decorDot}></div>
+                    <a href="https://www.polskieradio.pl/10/215" className={styles.navLink}>
+                        lifestyle i nauka
+                    </a>
+                    <div className={styles.decorDot}></div>
+                    <a href="https://www.polskieradio.pl/10/217" className={styles.navLink}>
+                        sport
+                    </a>
+                    <div className={styles.decorDot}></div>
+                    <a href="https://www.polskieradio.pl/169" className={styles.navLink}>
+                        blog muzyczny
+                    </a>
+                </nav>
+            }
         </div>
         <div className={styles.blackBelt}></div>
         </>
